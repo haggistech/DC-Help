19b8a145f7a429d58c7911dff1e50262f76e3438
haggistech <haggistech@gmail.com>
2021-01-03 15:56:20 +0000

    test

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /home/haggis/.config/docker/
-for i in */
+for dir in *
     do
-        zip -0 -r --exclude=*MediaCover* --exclude=*Backup* --exclude=*duplicati* "${i%}.zip" "$i" & done
+        zip -r --exclude=*MediaCover* --exclude=*Backup* --exclude=*duplicati* "${dir}.zip" "$dir" & done
     wait
 for f in $(ls /home/haggis/.config/docker/*.zip)
     do
-        aws s3 cp \$f s3://mik-plex-backups/configbackups/
+        aws s3 cp $f s3://mik-plex-backups/configbackups/
     done
 rm /home/haggis/.config/docker/*.zip
